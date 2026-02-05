89c7b331b844e1eb7bfb5b8826e7c3655ba76702
tlsalsckd13 <tlsalsckd13@naver.com>
2026-02-05 15:30:40 +0900

    버킷 이름 v2로 변경

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # 테스트용 S3 버킷 (이름은 전세계 유일해야 하므로 뒤에 숫자 등을 바꿔주세요)
 resource "aws_s3_bucket" "test_bucket" {
-  bucket = "minchang-terraform-test-20260205" 
+  bucket = "minchang-terraform-test-20260205-v2" 
   
   tags = {
     Name        = "Test bucket"
