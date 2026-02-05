4fd7d81f666cbe372595c2bc00d6b5afec3ce212
tlsalsckd13 <tlsalsckd13@naver.com>
2026-02-05 15:25:27 +0900

    테라폼 코드 업로드 (재시도)

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# 테스트용 S3 버킷 (이름은 전세계 유일해야 하므로 뒤에 숫자 등을 바꿔주세요)
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "minchang-terraform-test-20260205" 
+  
+  tags = {
+    Name        = "Test bucket"
+    Environment = "Dev"
+  }
+}
